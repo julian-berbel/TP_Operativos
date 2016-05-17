cb9ad278976a6797094e308c0782ab8f2c366531
Julian Freire <julislm.13@gmail.com>
2016-05-17 00:13:56 -0300

    Termino implementacion de multihilos en el UMC
    El UMC crea un "joineable" para el nucleo e
    hilos "detached" para los CPU.

--- umc/umc.c
+++ umc/umc.c
@@ -3,42 +3,45 @@
 #include"umc.h"
 
 int main(){
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_t thread_cpu;
+	pthread_t thread_nucleo;
 	abrirConfiguracion();
 	log_info(logger, "Inicia proceso UMC");
-
-	/*int socket_swap = crear_socket_cliente(ipSwap, puertoSwap);
-
-	log_info(logger_pantalla, "UMC y Swap conectados");*/
-
+	int socket_swap = crear_socket_cliente(ipSwap, puertoSwap);
+	log_info(logger_pantalla, "UMC y Swap conectados");
 	int socket_servidor = crear_socket_servidor(ipUmc, puertoUmc);
-
 	int socket_cliente = recibirConexion(socket_servidor);
-
 	char* mensaje;
-
 	while(string_is_empty(mensaje = recibir_string_generico(socket_cliente)));
-
-	//enviar_string(socket_swap, mensaje);
 	if (strcmp(mensaje, "n")){
-		int args_nucleo = 1234;
-		pthread_t thread_nucleo;
-		int r = pthread_create(&thread_nucleo, NULL, funcion_nucleo, (void*) args_nucleo);
-		printf("Recibi un mensaje de Kernel\n");
-
-
+		int *args_nucleo = malloc(sizeof(*args_nucleo));
+		if ( args_nucleo == NULL ) {
+			log_info(logger_pantalla, "No se pudo asignar memoria para un hilo del CPU");
+			cerrar_todo();
+		}
+		*args_nucleo = socket_cliente;
+		int r = pthread_create(&thread_nucleo, NULL, funcion_nucleo, args_nucleo);
+		//Hay que hacer el pthread_join en algun lugar del main. Posiblemente en el cerrar_todo.
 	}else if (strcmp(mensaje, "c")){
-		printf("Recibir un mensaje de CPU\n");
+		int *args_cpu = malloc(sizeof(*args_cpu));
+		if ( args_cpu == NULL){
+			cerrar_todo();
+		}
+		*args_cpu = socket_cliente;
+		pthread_create(&thread_cpu, &attr, funcion_cpu, args_cpu);
 	}
-
-
-
-
+	//Comienza el cierre del main.
 	free(mensaje);
-	//close(socket_swap);
+	close(socket_swap);
 	close(socket_servidor);
 	close(socket_cliente);
+	pthread_attr_destroy(&attr);
+	pthread_join(thread_nucleo, NULL);
+	pthread_cancel(thread_cpu);
 	cerrar_todo();
-
 	return 0;
 }
 
@@ -65,7 +68,6 @@ void abrirConfiguracion(){
 	printf("%d\n", marco_x_proc);
 	printf("%d\n", entradas_tlb);
 	printf("%d\n", retardo);
-
 }
 
 void cerrar_todo(){
@@ -74,14 +76,23 @@ void cerrar_todo(){
 	config_destroy(configuracionUMC);
 }
 
-void* funcion_nucleo(void *argumento){ // thread para nucleo
-	int socket_nucleo;
-	socket_nucleo = (int) argumento;
+void *funcion_nucleo(void *argumento){ // thread para nucleo
+	int socket_nucleo = *((int *) argumento);
 	char* mensaje = string_new();
 	string_append(&mensaje, "okey\n");
 	enviar_string(socket_nucleo, mensaje);
 	free(mensaje);
-
+	free(argumento);
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
 }
 
-
+void *funcion_cpu(void *argumento){
+	int socket_cpu = *((int *) argumento);
+	char* mensaje = string_new();
+	string_append(&mensaje, "okey\n");
+	enviar_string(socket_cpu, mensaje);
+	free(mensaje);
+	close(socket_cpu);
+	free(argumento);
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+}
--- umc/umc.h
+++ umc/umc.h
@@ -1,11 +1,12 @@
 #ifndef UMC_H_
 #define UMC_H_
 
+#include<stdlib.h>
 #include<stdio.h>
+#include<pthread.h>
 #include<commons/log.h>
 #include<commons/config.h>
 #include"servidor.h"
-#include <pthread.h>
 #include"cliente.h"
 
 #define RUTA_LOG "/home/utnso/umc.log"
@@ -27,6 +28,7 @@ t_log* logger_pantalla;
 
 void abrirConfiguracion();
 void cerrar_todo();
-void *funcion_nucleo();
+void *funcion_nucleo(void *argumento);
+void *funcion_cpu(void *argumento);
 
 #endif
