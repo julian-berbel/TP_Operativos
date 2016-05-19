4d4550f099517bfe5db33e830eb59699c3e6e86a
Kevin Nielsen <kevin.nielsen93@gmail.com>
2016-05-18 22:38:27 -0300

    Separo la creacion del archivo binario de la swap en otro archivo fuente (.c)
    Agregue una funcion para calcular el tamaño total de la swap en base al tamaño de pag y cantidad de pags

---
+++ swap/binFile.c
@@ -0,0 +1,33 @@
+/*
+ * binFile.c
+ *
+ *  Created on: 18/5/2016
+ *      Author: utnso
+ */
+#include"swap.h"
+#include"binFile.h"
+
+void crearArchivoBinario(char* nombre_data, int pagina_size, int cant_paginas) {
+	char* comando = string_new();
+	string_append(&comando, "dd if=/dev/zero ");
+	string_append(&comando, "of=");
+	string_append(&comando, RUTA_BINFILE);
+	string_append(&comando, nombre_data);
+	string_append(&comando, " bs=");
+	string_append(&comando, tamanioArchivo(pagina_size, cant_paginas));
+	string_append(&comando, " count=1");
+
+	system(comando);
+
+}
+
+char* tamanioArchivo(int pagina_tamanio, int cantidad_pags) {
+	int tamanioTotal;
+	static char tamanioTotalString[10]; //hasta 9 cifras de tamaño
+
+	tamanioTotal = pagina_tamanio * cantidad_pags;
+
+	sprintf(tamanioTotalString, "%d", tamanioTotal);
+	return tamanioTotalString;
+}
+
---
+++ swap/binFile.h
@@ -0,0 +1,21 @@
+/*
+ * binFile.h
+ *
+ *  Created on: 18/5/2016
+ *      Author: utnso
+ */
+
+#ifndef BINFILE_H_
+#define BINFILE_H_
+
+#include<stdio.h>
+#include<commons/log.h>
+#include<commons/config.h>
+
+#define RUTA_BINFILE "/home/utnso/tp-2016-1c-Hellfish-Group/swap/"
+
+
+void crearArchivoBinario(char* nombre_data,int pagina_size,int cant_paginas);
+char* tamanioArchivo(int pag_size,int cantidad);
+
+#endif /* BINFILE_H_ */
