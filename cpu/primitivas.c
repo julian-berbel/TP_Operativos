05ae4211f005ed447fd1d67e224aa179e305685a
Julian Freire <julislm.13@gmail.com>
2016-06-06 00:31:26 -0300

    Termino de agregar las definiciones de primitivas

--- cpu/primitivas.c
+++ cpu/primitivas.c
@@ -34,10 +34,46 @@ void asignar(t_puntero puntero, t_valor_variable variable) {
 		enviar_string(socket_umc, mensaje);
 }
 
+t_valor_variable obtenerValorCompartida(t_nombre_compartida variable){
+	return CONTENIDO_VARIABLE;
+}
+
+t_valor_variable asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor){
+	return CONTENIDO_VARIABLE;
+}
+
+void irAlLabel(t_nombre_etiqueta etiqueta){
+
+}
+
+void llamarSinRetorno(t_nombre_etiqueta etiqueta){
+
+}
+
+void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar){
+
+}
+
+void finalizar(){
+
+}
+
+void retornar(t_valor_variable retorno){
+
+}
+
 void imprimir(t_valor_variable valor) {
 	printf("Imprimir %d\n", valor);
 }
 
 void imprimirTexto(char* texto) {
 	printf("ImprimirTexto: %s", texto);
+}
+
+void wait(t_nombre_semaforo identificador_semaforo){
+
+}
+
+void signal(t_nombre_semaforo identificador_semaforo){
+
 }
