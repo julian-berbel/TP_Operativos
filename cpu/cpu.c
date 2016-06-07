18c0ef898285a60228bd0147b3d276d00294e0f1
Julian Freire <julislm.13@gmail.com>
2016-06-06 21:46:02 -0300

    Agrego la funcion definirVariable al CPU
    Ademas agrego las estructuras necesarias para manejar el stack del
    PCB desde el CPU.
    Borro primitivas.h y .c y lo meto todo en cpu.h y .c (para variables compartidas)
    Mas algunos otros cambios...

--- cpu/cpu.c
+++ cpu/cpu.c
@@ -56,15 +56,160 @@ char* obtener_instruccion(t_PCB * pcb){
 	int bytes_tamanio_instruccion = pcb->indiceCodigo[program_counter][1];
 	int num_pagina = byte_inicio_instruccion / tamanio_pagina;
 	int offset = byte_inicio_instruccion - (num_pagina * tamanio_pagina);
-	char* instruccion = pedir_instruccion_umc(num_pagina, offset, bytes_tamanio_instruccion);
+	char* instruccion;
+	char* continuacion_instruccion;
+	int bytes_a_leer_primera_pagina;
+	if (bytes_tamanio_instruccion > (tamanio_pagina * 2)){
+		printf("El tamanio de la instruccion es mayor al tamanio de pagina\n");
+	}
+	if ((offset + bytes_tamanio_instruccion) < tamanio_pagina){
+		instruccion = pedir_bytes_umc(num_pagina, offset, bytes_tamanio_instruccion);
+	} else {
+		bytes_a_leer_primera_pagina = tamanio_pagina - offset;
+		instruccion = pedir_bytes_umc(num_pagina, offset, bytes_a_leer_primera_pagina);
+		continuacion_instruccion = pedir_bytes_umc((num_pagina + 1), 0, (bytes_tamanio_instruccion - bytes_a_leer_primera_pagina));
+		string_append(&instruccion, continuacion_instruccion);
+		free(continuacion_instruccion);
+	}
 	return instruccion;
 	//Una vez que se usa la instruccion hay que hacer free
 }
 
-char* pedir_instruccion_umc(int num_pagina, int offset, int tamanio){
+char* pedir_bytes_umc(int num_pagina, int offset, int tamanio){
 	char* instruccion = string_new();
 	/*Aca hay que serializar el mensaje, mandarlo al UMC, y deserializar el mensaje de respuesta
 	y appendearla a instruccion*/
 	string_append(&instruccion, "Aca va la instruccion que recibo de la UMC\n");
 	return instruccion;
+}
+
+
+//Empiezo a definir primitivas
+
+t_puntero definirVariable(t_nombre_variable variable) {
+	int nodos_stack = list_size(pcb_actual->indiceStack);
+	int posicion_stack;
+	int cantidad_variables;
+	int cantidad_argumentos;
+	int encontre_valor = 1;
+	nodo_stack *nodo;
+	pos_mem *posicion_memoria;
+	t_variable *var;
+	for(posicion_stack = (nodos_stack - 1); posicion_stack >= 0; posicion_stack--){
+		nodo = list_get(pcb_actual->indiceStack, posicion_stack);
+		cantidad_variables = list_size(nodo->vars);
+		cantidad_argumentos = list_size(nodo->args);
+		if(cantidad_variables != 0){
+			var = list_get(nodo->vars, (cantidad_variables - 1));
+			posicion_memoria = var->dir_var;
+			posicion_stack = -1;
+			encontre_valor = 0;
+		} else if(cantidad_argumentos != 0){
+			posicion_memoria = list_get(nodo->args, (cantidad_argumentos - 1));
+			posicion_stack = -1;
+			encontre_valor = 0;
+		}
+	}
+
+	pos_mem* nueva_posicion_memoria = malloc(sizeof(pos_mem));
+	t_variable *nueva_variable = malloc(sizeof(t_variable));
+	nodo = list_get(pcb_actual->indiceStack, (nodos_stack - 1));
+	if(encontre_valor == 0){
+		if(((posicion_memoria->offset + posicion_memoria->size) + 4) > tamanio_pagina){
+			nueva_posicion_memoria->pagina = (posicion_memoria->pagina + 1);
+			nueva_posicion_memoria->offset = 0;
+			nueva_posicion_memoria->size = 4;
+			nueva_variable->nombre_var = variable;
+			nueva_variable->dir_var = nueva_posicion_memoria;
+			list_add(nodo->vars, nueva_variable);
+		} else {
+			nueva_posicion_memoria->pagina = posicion_memoria->pagina;
+			nueva_posicion_memoria->offset = (posicion_memoria->offset + posicion_memoria->size);
+			nueva_posicion_memoria->size = 4;
+			nueva_variable->nombre_var = variable;
+			nueva_variable->dir_var = nueva_posicion_memoria;
+			list_add(nodo->vars, nueva_variable);
+		}
+	} else {
+		if(tamanio_pagina < 4){
+			printf("Tamaño de pagina menor a 4 bytes\n");
+		} else {
+			nueva_posicion_memoria->pagina = (pcb_actual->cantidadPaginas + 1);
+			nueva_posicion_memoria->offset = 0;
+			nueva_posicion_memoria->size = 4;
+			nueva_variable->nombre_var = variable;
+			nueva_variable->dir_var = nueva_posicion_memoria;
+			list_add(nodo->vars, nueva_variable);
+		}
+	}
+
+	int posicion_serializada = (nueva_posicion_memoria->pagina * tamanio_pagina) + nueva_posicion_memoria->offset;
+	return posicion_serializada;
+}
+
+t_puntero obtenerPosicionVariable(t_nombre_variable variable) {
+	printf("Obtener posicion de %c\n", variable);
+	char *mensaje=string_new();
+	string_append(&mensaje,"umc: busque la variable\n");
+	enviar_string(socket_umc, mensaje);
+	return POSICION_MEMORIA;
+}
+
+t_valor_variable dereferenciar(t_puntero puntero) {
+	printf("Dereferenciar %d y su valor es: %d\n", puntero, CONTENIDO_VARIABLE);
+	char *mensaje=string_new();
+		string_append(&mensaje,"umc: dereferencie la variable\n");
+		enviar_string(socket_umc, mensaje);
+	return CONTENIDO_VARIABLE;
+}
+
+void asignar(t_puntero puntero, t_valor_variable variable) {
+	printf("Asignando en %d el valor %d\n", puntero, variable);
+	char *mensaje=string_new();
+		string_append(&mensaje,"umc: asigne la variable\n");
+		enviar_string(socket_umc, mensaje);
+}
+
+t_valor_variable obtenerValorCompartida(t_nombre_compartida variable){
+	return CONTENIDO_VARIABLE;
+}
+
+t_valor_variable asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor){
+	return CONTENIDO_VARIABLE;
+}
+
+void irAlLabel(t_nombre_etiqueta etiqueta){
+
+}
+
+void llamarSinRetorno(t_nombre_etiqueta etiqueta){
+
+}
+
+void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar){
+
+}
+
+void finalizar(){
+
+}
+
+void retornar(t_valor_variable retorno){
+
+}
+
+void imprimir(t_valor_variable valor) {
+	printf("Imprimir %d\n", valor);
+}
+
+void imprimirTexto(char* texto) {
+	printf("ImprimirTexto: %s", texto);
+}
+
+void wait(t_nombre_semaforo identificador_semaforo){
+
+}
+
+void signal(t_nombre_semaforo identificador_semaforo){
+
 }
--- cpu/cpu.h
+++ cpu/cpu.h
@@ -6,7 +6,7 @@
 #include <commons/config.h>
 #include <commons/log.h>
 #include <stdio.h>
-#include "primitivas.h"
+#include <parser/parser.h>
 #include <commons/collections/list.h>
 
 #define RUTA_LOG "/home/utnso/cpu.log"
@@ -22,6 +22,42 @@ typedef struct{
 	char* indiceEtiquetas;
 	t_list* indiceStack;
 } t_PCB;
+
+typedef struct{
+	int pagina;
+	int offset;
+	int size;
+} pos_mem;
+
+typedef struct{
+	t_list* args; //Lista de pos_mem's. Cada posicion representa un argumento en el orden de la lista
+	t_list* vars; // Lista de variables(struct)
+	int dir_retorno;
+	pos_mem* var_retorno;
+} nodo_stack;
+
+typedef struct{
+	char nombre_var;
+	pos_mem* dir_var;
+} t_variable;
+
+//Defino funciones de primitivas
+
+t_puntero definirVariable(t_nombre_variable variable);
+t_puntero obtenerPosicionVariable(t_nombre_variable variable);
+t_valor_variable dereferenciar(t_puntero puntero);
+void asignar(t_puntero puntero, t_valor_variable variable);
+t_valor_variable obtenerValorCompartida(t_nombre_compartida variable);
+t_valor_variable asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor);
+void irAlLabel(t_nombre_etiqueta etiqueta);
+void llamarSinRetorno(t_nombre_etiqueta etiqueta);
+void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar);
+void finalizar();
+void retornar(t_valor_variable retorno);
+void imprimir(t_valor_variable valor);
+void imprimirTexto(char* texto);
+void wait(t_nombre_semaforo identificador_semaforo);
+void signal(t_nombre_semaforo identificador_semaforo);
 
 AnSISOP_funciones functions = {
 		.AnSISOP_definirVariable		= definirVariable,
@@ -53,10 +89,13 @@ int socket_umc;
 t_log* logger;
 t_log* logger_pantalla;
 int tamanio_pagina;
+t_PCB *pcb_actual;
+const int CONTENIDO_VARIABLE = 20; //Borrar cuando esten todas las primitivas
+const int POSICION_MEMORIA = 0x10; //Borrar cuando esten todas las primitivas
 
 void abrirConfiguracion();
 void cerrar_todo();
 char* obtener_instruccion(t_PCB * pcb);
-char* pedir_instruccion_umc(int num_pagina, int offset, int tamanio);
+char* pedir_bytes_umc(int num_pagina, int offset, int tamanio);
 
 #endif
